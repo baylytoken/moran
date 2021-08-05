453c28731d6ee3e7dd20e423f1869fe1a6e3f629
baylytoken <81519721+baylytoken@users.noreply.github.com>
2021-08-05 16:20:25 +0300

    Add files via upload
    first commint of moran

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+
+
+
+
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def index():
+    return render_template ("index.html")
+
+
+
+@app.route("/about")
+def about():
+    return render_template ("about.html")
+
+
+
+@app.route("/courses")
+def courses():
+    return render_template ("courses.html")
+
+
+@app.route("/lang")
+def lang():
+    return render_template ("lang.html")
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
